4cebee44d38ff4727c41aa643e820491823d41c9
Lami <Halam.Lee.Lami@gmail.com>
2021-08-04 02:12:50 +0900

    조건문 공부

--- JavaScript/momentum/app.js
+++ JavaScript/momentum/app.js
@@ -1,1 +1,9 @@
-console.log(3);
+const age = parseInt(prompt("How old are you?"));
+
+if (isNaN(age)) {
+    console.log("Please write a number");
+} else if (age < 18){
+    console.log("You are too young.");
+} else {
+    console.log("You can drink");
+}
