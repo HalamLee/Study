ad37c727f193ee73c450f9ca891bf0f774ef5ca2
Lami <Halam.Lee.Lami@gmail.com>
2021-08-11 14:09:25 +0900

    More Events

--- JavaScript/momentum/app.js
+++ JavaScript/momentum/app.js
@@ -1,17 +1,39 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleTitleClick(){
-    console.log("title was clicked!");
+function handleh1Click(){
+    console.log("h1 was clicked!");
 }
 
 function handleMouseEnter(){
-    title.innerText = "Mouse is here!";
+    h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave(){
-    title.innerText = "Mouse is gone!";
+    h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline(){
+    alert("ALL GOOOOOOD");
+}
+
+h1.addEventListener("click", handleh1Click);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.onmouseleave = handleMouseLeave;
+
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
