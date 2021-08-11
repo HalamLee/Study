74388a70957c62dd2e89c5ac624048e8dcb7f4b1
Lami <Halam.Lee.Lami@gmail.com>
2021-08-11 14:21:58 +0900

    CSS in Javascript

--- JavaScript/momentum/app.js
+++ JavaScript/momentum/app.js
@@ -1,39 +1,15 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleh1Click(){
-    console.log("h1 was clicked!");
+
+function handleTitleClick(){
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    
+    } else {
+    newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
-
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOOOOOD");
-}
-
-h1.addEventListener("click", handleh1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.onmouseleave = handleMouseLeave;
-
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
