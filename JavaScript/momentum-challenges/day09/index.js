cd5f0f7e1e006dbcfa7f0b4e7768dd4213e6c639
Lami <Halam.Lee.Lami@gmail.com>
2021-09-01 00:28:55 +0900

    string형을 int형으로 바꿈(parseInt 사용)

--- JavaScript/momentum-challenges/day09/index.js
+++ JavaScript/momentum-challenges/day09/index.js
@@ -1,19 +1,18 @@
 const maxNum = document.getElementById("maxNum");
 const inputGuess = document.getElementById("guessNumber");
 const form = document.getElementById("numberForm");
-const btn = document.getElementById("btn-play");
 const text1 = document.getElementById("text1");
 const text2 = document.getElementById("text2");
 
 function playBtn(event) {
   event.preventDefault();
-  const maxNumValue = maxNum.value;
-  const guessValue = inputGuess.value;
+  const maxNumValue = parseInt(maxNum.value);
+  const guessValue = parseInt(inputGuess.value);
   const machineNum = Math.floor(Math.random() * maxNumValue);
 
   text1.innerText = `You chose: ${guessValue}, the machine chose: ${machineNum}`;
-  if (guessValue == machineNum) {
-    text2.innerText = "You Win!";
+  if (guessValue === machineNum) {
+    text2.innerText = "You Won!";
   } else {
     text2.innerText = "You lost!";
   }
