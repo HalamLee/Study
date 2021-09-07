1f05a7ccfab9dcc5e3dc0142d910632538fffffa
Lami <Halam.Lee.Lami@gmail.com>
2021-09-07 18:29:50 +0900

    #2.1 Reusable Components with JSX + Props

--- React/movie/src/App.js
+++ React/movie/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <h1>hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="hamburger" />
+      <Food fav="bread" />
+    </div>
+  );
 }
 
 export default App;
