e1b60d82460ac8004a653b0aa081e144383d1806
Lami <Halam.Lee.Lami@gmail.com>
2021-09-17 00:02:01 +0900

    ~#3.0

--- React/movie/src/App.js
+++ React/movie/src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
-}
-
-function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="hamburger" />
-      <Food fav="bread" />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
