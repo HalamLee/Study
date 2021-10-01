396ad03593e653396591a6ad6f4f3d7625087095
Lami <Halam.Lee.Lami@gmail.com>
2021-10-01 20:37:55 +0900

    finish

--- JavaScript/momentum/js/greetings.js
+++ JavaScript/momentum/js/greetings.js
@@ -1,6 +1,9 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const todoForm = document.querySelector("#todo-form");
+const todoList = document.querySelector("#todo-list");
+const form = document.querySelector(".form");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -8,6 +11,7 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  todoForm.classList.remove(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
@@ -29,6 +33,9 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
+  // todoList.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem("todos", []);
 } else {
   paintGreetings(savedUsername);
+  todoForm.classList.remove(HIDDEN_CLASSNAME);
 }
