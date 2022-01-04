d06500570042bc115ef6a11dedc465eff1b3fc72
Lami <87893624+HalamLee@users.noreply.github.com>
2022-01-04 14:40:33 +0900

    useEffect

--- React/basic-movie/src/App.js
+++ React/basic-movie/src/App.js
@@ -1,30 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const onSubmit = (event) => {
-    event.preventDefault();
-    console.log(username, password);
-  };
+  const [count, setCount] = useState(0);
+  const [lami, setLami] = useState(0);
+  useEffect(() => {
+    console.log('count', count, 'lami', lami);
+  }, [count, lami]);
 
+  useEffect(() => {
+    console.log('first rendering');
+  }, []);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
-      <form onSubmit={onSubmit}>
-        <input
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <br />
-        <input
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <button type="submit">Login</button>
-      </form>
+      <h1>Lami</h1>
+      <button onClick={increment}>Click</button>
+      <button onClick={() => setLami(lami + 1)}>Click1</button>
     </div>
   );
 }
