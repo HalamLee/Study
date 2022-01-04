7749362b360fa8711ea1984f87295e3310aa83d6
Lami <87893624+HalamLee@users.noreply.github.com>
2022-01-04 15:11:08 +0900

    컴포넌트로 반복 제거

--- React/basic-movie/src/App.js
+++ React/basic-movie/src/App.js
@@ -1,24 +1,12 @@
-import React, { useEffect, useState } from 'react';
-
+import React from 'react';
+import Counter from './components/Counter';
 function App() {
-  const [count, setCount] = useState(0);
-  const [lami, setLami] = useState(0);
-  useEffect(() => {
-    console.log('count', count, 'lami', lami);
-  }, [count, lami]);
-
-  useEffect(() => {
-    console.log('first rendering');
-  }, []);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
   return (
     <div className="App">
       <h1>Lami</h1>
-      <button onClick={increment}>Click</button>
-      <button onClick={() => setLami(lami + 1)}>Click1</button>
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
