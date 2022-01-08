94ae3c1950d027a75030e851ebc03dd46b331bd4
Lami <87893624+HalamLee@users.noreply.github.com>
2022-01-09 02:27:19 +0900

    영화 리스트 추가 폼 만들기

--- React/basic-movie/src/App.js
+++ React/basic-movie/src/App.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import Movie from './components/Movie';
 function App() {
-  const movies = [
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieYear, setMovieYear] = useState('');
+  const [movies, setMovies] = useState([
     { title: 'Lami1', year: 2001 },
     { title: 'Lami2', year: 2002 },
     { title: 'Lami3', year: 2003 },
-  ];
+  ]);
+
   const renderMovies = movies.map((movie) => {
     return <Movie movie={movie} key={movie.title} />;
   });
+  const addMovie = (event) => {
+    event.preventDefault();
+    setMovies([
+      ...movies,
+      {
+        title: movieTitle,
+        year: movieYear,
+      },
+    ]);
+    setMovieTitle('');
+    setMovieYear('');
+  };
 
   return (
     <div className="App">
       <h1>Movie list</h1>
+      <form onSubmit={addMovie}>
+        <input
+          type="text"
+          value={movieTitle}
+          placeholder="영화제목"
+          onChange={(e) => setMovieTitle(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          value={movieYear}
+          placeholder="개봉연도"
+          onChange={(e) => setMovieYear(e.target.value)}
+        />
+        <br />
+        <button type="submit">영화추가</button>
+      </form>
       {renderMovies}
     </div>
   );
