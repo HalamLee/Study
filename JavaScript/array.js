34f095b505a8c60e5ae355180a9fb0acefa5f62a
Lami <87893624+HalamLee@users.noreply.github.com>
2022-01-14 01:40:32 +0900

    Array 개념과 APIs

--- JavaScript/array.js
+++ JavaScript/array.js
@@ -7,27 +7,81 @@ console.log(arr1); // []
 console.log(arr2); // [1, 2]
 
 // 2. Index 통해서 배열에 접근
-const fruits = ['apple', 'kiwi'];
-console.log(fruits); // ['apple', 'kiwi']
+const fruits = ['🍎', '🥝'];
+console.log(fruits); // ['🍎', '🥝']
 console.log(fruits.length); // 2
-console.log(fruits[0]); // apple
-console.log(fruits[fruits.length - 1]); // kiwi
+console.log(fruits[0]); // 🍎
+console.log(fruits[fruits.length - 1]); // 🥝
 
 // 3. 배열에서 반복문 사용
 // 3-1. for문
 for (let i = 0; i < fruits.length; i++) {
-  console.log(fruits[i]); // apple, kiwi
+  console.log(fruits[i]); // 🍎, 🥝
 }
 
 // 3-2. for of
 for (let fruit of fruits) {
-  console.log(fruit); // apple, kiwi
+  console.log(fruit); // 🍎, 🥝
 }
 
 // 3-3. forEach
 // forEach(callbackfn: (value: T, index: number, array: T[]) => void, thisArg?: any): void;
 fruits.forEach((fruit, index, array) => {
-  console.log(fruit); // apple, kiwi
-  console.log(fruit, index); // apple 0, kiwi 1
-  console.log(fruit, index, array); // apple 0 ['apple', 'kiwi'], kiwi 1 ['apple', 'kiwi']
+  console.log(fruit); // 🍎, 🥝
+  console.log(fruit, index); // 🍎 0, 🥝 1
+  console.log(fruit, index, array); // 🍎 0 ['🍎', '🥝'], 🥝 1 ['🍎', '🥝']
 });
+
+// 4. 추가, 삭제, 복사
+// push: 배열 맨끝에 값 추가
+fruits.push('🍇', '🍑');
+console.log(fruits); // ['🍎', '🥝', '🍇', '🍑']
+
+// pop: 배열 맨끝에 있는 값 삭제
+fruits.pop();
+console.log(fruits); // ['🍎', '🥝', '🍇']
+
+// unshift: 배열 맨앞에 값 추가
+fruits.unshift('🍓', '🍋');
+console.log(fruits); // ['strawberry', '🍋', '🍎', '🥝', '🍇']
+
+// shift: 배열 맨앞에 있는 값 삭제
+fruits.shift();
+console.log(fruits); // ['🍋', '🍎', '🥝', '🍇']
+
+// unshift와 shift는 push와 pop보다 실행속도가 느림
+// => 뒤에 추가, 삭제하는 것은 아직 값이 배정되지 않았기 때문에 그냥 추가하면 되지만, 앞에 추가하거나 삭제할 때는 다른 요소들의 변화도 이루어지기 때문
+// 그러므로 push와 pop 사용하기
+
+fruits.push('🍌', '🍉');
+console.log(fruits); // ['🍋', '🍎', '🥝', '🍇', '🍌', '🍉']
+
+// splice: 지정한 위치에 있는 값 삭제
+fruits.splice(1, 1); // 1번째 인덱스 하나 삭제
+console.log(fruits); // ['🍋', '🥝', '🍇', '🍌', '🍉']
+
+fruits.splice(1, 1, '🍒', '🍈'); // 1번째 인덱스 삭제하고 그 자리에 값 추가
+console.log(fruits); // ['🍋',  '🍒', '🍈', '🍇', '🍌', '🍉']
+
+fruits.splice(1); // 1번째 인덱스부터 끝까지 삭제
+console.log(fruits); // ['🍋']
+
+// concat: 두 가지 배열 합치기
+const fruits2 = ['🍉', '🍏'];
+const newFruits = fruits.concat(fruits2);
+console.log(newFruits); // ['🍋', '🍉', '🍏']
+
+// 5. 검색
+// indexOf(): 해당 값이 처음 나오는 게 몇 번째 인덱스인지, 만약 해당 값이 없다면 -1 return
+console.log(newFruits.indexOf('🍉')); // 1
+console.log(newFruits.indexOf('🍓')); // -1
+
+// lastIndexOf(): 해당 값이 마지막으로 나오는 게 몇 번째 인덱스인지, 만약 해당 값이 없다면 -1 return
+newFruits.push('🍉');
+console.log(newFruits); // ['🍋', '🍉', '🍏', '🍉]
+console.log(newFruits.lastIndexOf('🍉')); // 3
+console.log(newFruits.lastIndexOf('🍓')); // -1
+
+// includes(): 배열에 해당 값이 있는지 boolean으로 return
+console.log(newFruits.includes('🍓')); // false
+console.log(newFruits.includes('🍋')); // true
