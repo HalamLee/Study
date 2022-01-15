269855ae0eee1c776a875d1ad4a6e8813bfcdb5b
Lami <87893624+HalamLee@users.noreply.github.com>
2022-01-16 01:55:21 +0900

    action

---
+++ JavaScript/redux-vanilla/store.js
@@ -0,0 +1,10 @@
+const redux = require('redux');
+
+// actions
+// action-types
+const ADD_SUBSCRIBER = 'ADD_SUBSCRIBER';
+const addSubscriber = () => {
+  return {
+    type: ADD_SUBSCRIBER,
+  };
+};
