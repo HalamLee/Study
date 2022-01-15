3d0167de957c53f11445ba8aea28af68d0c87bc7
Lami <87893624+HalamLee@users.noreply.github.com>
2022-01-16 02:03:37 +0900

    reducer

--- JavaScript/redux-vanilla/store.js
+++ JavaScript/redux-vanilla/store.js
@@ -1,3 +1,5 @@
+// store.js
+
 const redux = require('redux');
 
 // actions
@@ -7,4 +9,25 @@ const addSubscriber = () => {
   return {
     type: ADD_SUBSCRIBER,
   };
+};
+
+// reducers
+const initialState = {
+  // state 초기 값
+  subscribers: 365,
+};
+const reducer = (state = initialState, action) => {
+  // state 없으면 설정한 초기 값 사용
+  switch (
+    action.type // switch문을 통해 핸들링
+  ) {
+    case ADD_SUBSCRIBER: // action의 type이 ADD_SUBSCRIBER라면
+      return {
+        ...state, // state 값
+        subscribers: state.subscribers + 1, // state의 subscribers 1 증가
+      };
+    default:
+      // action의 type이 ADD_SUBSCRIBER가 아니라면 state 반환
+      return state;
+  }
 };
