3464460231028ae122f4e6a1ca341087483f8242
Lami <87893624+HalamLee@users.noreply.github.com>
2022-01-16 19:41:40 +0900

    redux-logger 사용

--- JavaScript/redux-vanilla/store.js
+++ JavaScript/redux-vanilla/store.js
@@ -1,6 +1,8 @@
-// store.js
-
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 // actions
 // action-types
@@ -31,3 +33,18 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+// store
+const store = createStore(reducer, applyMiddleware(logger));
+
+// subscribe - view - dispatch
+
+// store.subscribe(() => {
+//   console.log('subscribe ===>>', store.getState());
+// });
+
+store.dispatch(addSubscriber());
+store.dispatch(addSubscriber());
+store.dispatch(addSubscriber());
+store.dispatch(addSubscriber());
+store.dispatch(addSubscriber());
