9f5f0e9484fb78de2586da2062af2b0596fb0a95
Lami <87893624+HalamLee@users.noreply.github.com>
2022-01-18 14:10:31 +0900

    add: viewsReducer

---
+++ React/practice-redux/src/redux/views/reducer.js
@@ -0,0 +1,18 @@
+import { ADD_VIEW } from './types';
+const initalState = {
+  count: 0,
+};
+
+const viewsReducer = (state = initalState, action) => {
+  switch (action.type) {
+    case ADD_VIEW:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    default:
+      return state;
+  }
+};
+
+export default viewsReducer;
