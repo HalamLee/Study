b44ab1258a6f7508f5a0be772d084cd503ac7334
Lami <Halam.Lee.Lami@gmail.com>
2022-01-22 18:11:34 +0900

    조건부 렌더링

--- React/react-exam/react-exam1/src/App.js
+++ React/react-exam/react-exam1/src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
+import React from 'react';
+import MyHeader from './MyHeader';
 
 function App() {
+  const style = {
+    App: {
+      backgroundColor: 'black',
+      textAlign: 'center',
+    },
+    h2: {
+      color: 'red',
+    },
+    bold_text: {
+      color: 'green',
+    },
+  };
+
+  const name = 'Lami';
+  const number = 5;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>안녕 리액트</h2>
-      </header>
+    <div style={style.App}>
+      <MyHeader />
+      <h2 style={style.h2}>안녕 리액트 {name}</h2>
+      <b style={style.bold_text}>
+        {number}는 : {number % 2 === 0 ? '짝수' : '홀수'}
+      </b>
     </div>
   );
 }
