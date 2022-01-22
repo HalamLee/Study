183571f556d9ac6e5a26e0df75ecc69a45f6501b
Lami <Halam.Lee.Lami@gmail.com>
2022-01-22 18:38:44 +0900

    state

--- React/react-exam/react-exam1/src/App.js
+++ React/react-exam/react-exam1/src/App.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import MyHeader from './MyHeader';
+import Counter from './Counter';
+import style from './App.css';
 
 function App() {
-  const style = {
-    App: {
-      backgroundColor: 'black',
-      textAlign: 'center',
-    },
-    h2: {
-      color: 'red',
-    },
-    bold_text: {
-      color: 'green',
-    },
-  };
-
-  const name = 'Lami';
   const number = 5;
 
   return (
-    <div style={style.App}>
-      <MyHeader />
-      <h2 style={style.h2}>안녕 리액트 {name}</h2>
-      <b style={style.bold_text}>
-        {number}는 : {number % 2 === 0 ? '짝수' : '홀수'}
-      </b>
+    <div className="App">
+      {/* <MyHeader /> */}
+      <Counter />
     </div>
   );
 }
