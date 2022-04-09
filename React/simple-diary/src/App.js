60f9a8d244fa19b5ed2e76c7f78d0fad527e40cf
Lami <Halam.Lee.Lami@gmail.com>
2022-04-09 23:17:59 +0900

    Add: create diary data

--- React/simple-diary/src/App.js
+++ React/simple-diary/src/App.js
@@ -1,51 +1,68 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-const dummyList = [
-  {
-    id: 1,
-    author: 'Lami',
-    content: 'hi',
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: 'Hong',
-    content: 'sad',
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: 'Lee',
-    content: 'good',
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: 'Kim',
-    content: 'not bad',
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 5,
-    author: 'Park',
-    content: 'umm..',
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: 'Lami',
+//     content: 'hi',
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: 'Hong',
+//     content: 'sad',
+//     emotion: 1,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: 'Lee',
+//     content: 'good',
+//     emotion: 4,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: 'Kim',
+//     content: 'not bad',
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 5,
+//     author: 'Park',
+//     content: 'umm..',
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
