101bbb7f66789415332d5a7834cd4813674d71db
Lami <Halam.Lee.Lami@gmail.com>
2022-04-22 01:48:22 +0900

    add: useNavigate를 이용한 page moving

--- React/emotion-diary/src/pages/Edit.js
+++ React/emotion-diary/src/pages/Edit.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
 const Edit = () => {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const id = searchParams.get('id');
@@ -16,6 +17,18 @@ const Edit = () => {
       <p>이곳은 일기 수정페이지 입니다.</p>
       <button onClick={() => setSearchParams({ who: 'lami' })}>
         Query String 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          navigate('/home');
+        }}>
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}>
+        뒤로가기
       </button>
     </div>
   );
