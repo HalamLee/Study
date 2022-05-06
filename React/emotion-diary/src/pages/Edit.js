c783308b6d4352f595708740fe03450b3fe1a192
Lami <Halam.Lee.Lami@gmail.com>
2022-05-06 23:48:59 +0900

    add: useParams를 이용한 수정 페이지 동적 라우팅

--- React/emotion-diary/src/pages/Edit.js
+++ React/emotion-diary/src/pages/Edit.js
@@ -1,35 +1,14 @@
 import React from 'react';
-import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Edit = () => {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const id = searchParams.get('id');
-  console.log('id', id);
-
-  const mode = searchParams.get('mode');
-  console.log('mode', mode);
+  const { id } = useParams();
+  console.log(id);
 
   return (
     <div>
-      <h1>Edit</h1>
-      <p>이곳은 일기 수정페이지 입니다.</p>
-      <button onClick={() => setSearchParams({ who: 'lami' })}>
-        Query String 바꾸기
-      </button>
-      <button
-        onClick={() => {
-          navigate('/home');
-        }}>
-        HOME으로 가기
-      </button>
-      <button
-        onClick={() => {
-          navigate(-1);
-        }}>
-        뒤로가기
-      </button>
+      <h2>Edit</h2>
     </div>
   );
 };
