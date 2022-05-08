362e0b9422b2eb9f8203c928c613db227b0e07c4
Lami <Halam.Lee.Lami@gmail.com>
2022-05-08 22:42:44 +0900

    add: 일기 상세페이지 - 주소창을 통해 없는 일기 보려 할 때 alert 띄우고 접근 못하도록 막기

--- React/emotion-diary/src/pages/Diary.js
+++ React/emotion-diary/src/pages/Diary.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { DiaryStateContext } from '../App';
 
 const Diary = () => {
   const { id } = useParams();
-  console.log('id :>> ', id);
+  const diaryList = useContext(DiaryStateContext);
+  const navigate = useNavigate();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+
+      if (targetDiary) {
+        // 일기가 존재할 때
+        setData(targetDiary);
+      } else {
+        // 일기가 없을 때
+        alert('없는 일기입니다.');
+        navigate('/', { replace: true });
+      }
+    }
+  }, [id, diaryList]);
 
   return (
     <div>
