e76e8c8f359b4dcb6d397a7a84dc0fa6c8d23547
Lami <Halam.Lee.Lami@gmail.com>
2022-05-19 23:55:13 +0900

    add: 구조분해할당을 사용해 배열 요소에 접근하기

---
+++ JavaScript/clean-code-JS/배열-다루기/배열-요소에-접근하기.js
@@ -0,0 +1,32 @@
+function clickGroupButton() {
+  const confirmButton = document.getElementsByTagName('button');
+  const cancleButton = document.getElementsByTagName('button');
+  const resetButton = document.getElementsByTagName('button');
+}
+
+// 구조분해할당을 통해 더 직관적이고 가독성 좋게 나타낼 수 있다.
+
+function clickGroupButton2() {
+  const [confirmButton, cancleButton, resetButton] =
+    document.getElementsByTagName('button');
+}
+
+// ------------------------------------------------------------------------
+
+function formatDate(targetDate) {
+  const date = targetDate.toISOString().split('T')[0]; // 0번째 인덱스 활용 -> 구조분해할당을 사용하자
+
+  const [year, month, day] = date.split('-');
+
+  return `${year}년 ${month}월 ${day}일`;
+}
+
+// 하나만 사용할 때도 구조분해할당을 사용하자
+
+function formatDate(targetDate) {
+  const [date] = targetDate.toISOString().split('T'); // 구조분해할당 사용
+
+  const [year, month, day] = date.split('-');
+
+  return `${year}년 ${month}월 ${day}일`;
+}
