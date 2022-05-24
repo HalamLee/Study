83164a89dbbd754871ea4613e0589b6cae7e3c78
Lami <Halam.Lee.Lami@gmail.com>
2022-05-24 21:43:16 +0900

    add: map vs forEach

---
+++ JavaScript/clean-code-JS/배열-다루기/map과forEach.js
@@ -0,0 +1,26 @@
+const prices = ['1000', '2000', '3000'];
+
+const newPricesForEach = prices.forEach((price) => price + '원');
+const newPricesMap = prices.map((price) => price + '원');
+
+// -------------------------------------------------------------------
+const newPricesForEach2 = prices.forEach(function (price) {
+  return price + '원';
+});
+const newPricesMap2 = prices.map(function (price) {
+  return price + '원';
+});
+
+console.log(newPricesForEach); // undefined
+console.log(newPricesMap); // [ '1000원', '2000원', '3000원' ]
+
+// -------------------------------------------------------------------------
+// forEach는 새로운 배열을 return 하는 것이 아니라 주어진 함수를 배열 요소 각각에 대해 실행하는 반복문의 개념
+// forEach는 undefined를 return 한다.
+
+// map은 주어진 함수를 배열 요소 각각에 대해 실행하며 새로운 배열을 만들어 return 한다.
+// 원본 배열 손상x
+// ---------------------------------------------------------------------------------------------------------------
+
+prices.forEach((price) => console.log(price + '원'));
+prices.map((price) => console.log(price + '원'));
