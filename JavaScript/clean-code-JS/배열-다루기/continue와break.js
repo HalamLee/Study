08a819643d597f8f64a676433cd4b962243c5caf
Lami <Halam.Lee.Lami@gmail.com>
2022-05-26 22:34:23 +0900

    add: continue와 break

---
+++ JavaScript/clean-code-JS/배열-다루기/continue와break.js
@@ -0,0 +1,12 @@
+const orders = ['first', 'second', 'third'];
+
+// orders.forEach(function(order){
+//   if(order === 'second') {
+//     continue;
+//     break;
+//   }
+//   console.log(order);
+// })
+
+// forEach에서는 continue, break 사용 불가능 => 예외처리를 해야 함.
+// 조기에 반복을 종료하려면 forEach보다는 for문, for...of, for...in 등을 사용하자.
